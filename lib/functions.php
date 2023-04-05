c39371faf541e1b685674f5c9b5c2cde8ada7234
Pam5204 <pyu2@njit.edu>
2023-04-05 14:53:14 -0400

    enhancemnt

--- lib/functions.php
+++ lib/functions.php
@@ -3,7 +3,12 @@
 require_once(__DIR__ . "/db.php");
 //This is going to be a helper for redirecting to our base project path since it's nested in another folder
 //This MUST match the folder name exactly
+<<<<<<< HEAD
 $BASE_PATH = '/Project';
+=======
+$BASE_PATH = '/Project/';
+//we moved the flash require higher so following functions have access to it
+>>>>>>> 09d38254e941b144051ec21e5caab5ca1293a8b6
 //TODO 4: Flash Message Helpers
 require(__DIR__ . "/flash_messages.php");
 
@@ -20,6 +25,9 @@ require(__DIR__ . "/user_helpers.php");
 require(__DIR__ . "/duplicate_user_details.php");
 //reset session
 require(__DIR__ . "/reset_session.php");
+<<<<<<< HEAD
 
 require(__DIR__ . "/get_url.php");
+=======
+>>>>>>> 09d38254e941b144051ec21e5caab5ca1293a8b6
 ?>
